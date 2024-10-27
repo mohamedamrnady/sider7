3373c7e3fdb55685316f99b6b938564e74def2eb
Nady <mohamed.amr.nes@gmail.com>
2024-10-27 23:17:48 +0300

    VS2022

--- setenv.cmd
+++ setenv.cmd
@@ -1,5 +1,5 @@
 @echo off
 echo Setting kitserver compile environment
-@call "c:\program files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsx86_amd64.bat"
+@call "E:\Visual Studio\App\VC\Auxiliary\Build\vcvarsall.bat"
 echo Environment set
 
